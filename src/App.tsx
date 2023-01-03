97da8c44085fbc6bb9cc7e93bb7562a8e3712893
Rodrigo Magaldi <rod.magaldi@gmail.com>
2023-01-03 17:01:29 -0300

    Add header component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styles from "./App.module.css";
+import { Header } from "./components/Header";
+
+import "./global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
