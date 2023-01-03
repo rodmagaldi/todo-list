2b4eed996a7282f76eb703609beb1e724d5e1494
Rodrigo Magaldi <rod.magaldi@gmail.com>
2023-01-03 18:11:55 -0300

    task input form layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from "./App.module.css";
+import { PlusCircle } from "phosphor-react";
 import { Header } from "./components/Header";
 
 import "./global.css";
@@ -7,6 +8,17 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className={styles.bodyBox}>
+        <div className={styles.contentBox}>
+          <form className={styles.inputBox}>
+            <input type="text" placeholder="Adicione uma nova tarefa" />
+            <button>
+              Criar
+              <PlusCircle size={16} />
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
