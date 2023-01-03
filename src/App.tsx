3ec4a27bbf00b7cd438f939e60afd5b8b2f79e28
Rodrigo Magaldi <rod.magaldi@gmail.com>
2023-01-03 19:01:11 -0300

    add task

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,40 @@ import { PlusCircle } from "phosphor-react";
 import { Header } from "./components/Header";
 
 import "./global.css";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface Task {
+  content: string;
+  done: boolean;
+}
 
 function App() {
+  const [newTaskContent, setNewTaskContent] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleNewTaskChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNewTaskContent(event.target.value);
+  };
+
+  const handleAddTask = (event: FormEvent) => {
+    event.preventDefault();
+    setTasks([...tasks, { content: newTaskContent, done: false }]);
+    setNewTaskContent("");
+  };
+
   return (
     <div className="App">
       <Header />
       <div className={styles.bodyBox}>
         <div className={styles.contentBox}>
-          <form className={styles.inputBox}>
-            <input type="text" placeholder="Adicione uma nova tarefa" />
+          <form onSubmit={handleAddTask} className={styles.inputBox}>
+            <input
+              type="text"
+              placeholder="Adicione uma nova tarefa"
+              value={newTaskContent}
+              onChange={handleNewTaskChange}
+              required
+            />
             <button>
               Criar
               <PlusCircle size={16} />
